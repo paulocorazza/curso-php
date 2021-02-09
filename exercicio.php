42fc6c5cd783963d3fdef55a59b6c688f41e82dd
Paulo Corazza <corazza.paulovinicius@gmail.com>
2021-02-08 22:54:35 -0300

    começando php

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="recursos/css/exercicio.css">
+    <title>Curso PHP</title>
+</head>
+
+<body class="exercicio" >
+    <header class="cabecalho">
+        <h1>Exercícios PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem Formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+           
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS 	© <?=  date('Y'); ?>
+    </footer>
+</body>
+
+</html> 
